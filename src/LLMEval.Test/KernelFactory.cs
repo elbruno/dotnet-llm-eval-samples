6ba3032ff7387fea2a4e2b21e381a23c1980cbe5
Bruno Capuano <bcapuano@gmail.com>
2024-04-26 12:54:57 -0400

    move kernel creation to new class

---
+++ src/LLMEval.Test/KernelFactory.cs
@@ -0,0 +1,34 @@
+﻿#pragma warning disable SKEXP0001, SKEXP0003, SKEXP0010, SKEXP0011, SKEXP0050, SKEXP0052
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+namespace LLMEval.Test;
+
+internal static class KernelFactory
+{
+    public static Kernel CreateAndConfigureKernelTest()
+    {
+        var builder = Kernel.CreateBuilder();
+        builder.AddOpenAIChatCompletion(
+            modelId: "phi3",
+            endpoint: new Uri("http://localhost:11434"),
+            apiKey: "api");
+
+        return builder.Build();
+    }
+
+    public static Kernel CreateAndConfigureKernelEval()
+    {
+        var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+        var builder = Kernel.CreateBuilder();
+
+        builder.AddAzureOpenAIChatCompletion(
+            config["AZURE_OPENAI_MODEL"],
+            config["AZURE_OPENAI_ENDPOINT"],
+            config["AZURE_OPENAI_KEY"]);
+
+        return builder.Build();
+    }
+
+}
--- src/LLMEval.Test/Program.cs
+++ src/LLMEval.Test/Program.cs
@@ -1,50 +1,20 @@
-﻿#pragma warning disable SKEXP0001, SKEXP0003, SKEXP0010, SKEXP0011, SKEXP0050, SKEXP0052
-
-using Microsoft.Extensions.Configuration;
-using Microsoft.SemanticKernel;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Trace;
+﻿using Microsoft.SemanticKernel;
 using LLMEval.Core;
 using LLMEval.Data;
 using LLMEval.Output;
+using LLMEval.Test;
 
 namespace LLMEval;
 
 class Program
 {
-    private static Kernel CreateAndConfigureKernelTest()
-    {       
-        var builder = Kernel.CreateBuilder();
-        builder.AddOpenAIChatCompletion(
-            modelId: "phi3", 
-            endpoint: new Uri("http://localhost:11434"), 
-            apiKey:"api");
-
-        return builder.Build();
-    }
-
-    private static Kernel CreateAndConfigureKernelEval()
-    {
-        var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
-
-        var builder = Kernel.CreateBuilder();
-
-        builder.AddAzureOpenAIChatCompletion(
-            config["AZURE_OPENAI_MODEL"],
-            config["AZURE_OPENAI_ENDPOINT"],
-            config["AZURE_OPENAI_KEY"]);
-
-        return builder.Build();
-    }
-
-
     static async Task Main()
     {
         Console.WriteLine($"Creating kernels ...");
 
         // create kernels
-        var kernelEval = CreateAndConfigureKernelEval();
-        var kernelTest = CreateAndConfigureKernelTest();
+        var kernelEval = KernelFactory.CreateAndConfigureKernelEval();
+        var kernelTest = KernelFactory.CreateAndConfigureKernelTest();
 
         SpectreConsoleOutput.DisplayKernels(kernelTest, kernelEval);
 
